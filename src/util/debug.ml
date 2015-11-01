98ffb30a3c3f8f9b146d0a877d7e43ae7d6b19c9
Akira Baruah <akira.baruah@gmail.com>
2015-11-01 06:11:12 +0000

    Add case for Seq type in Debug.string_of_expr

--- src/util/debug.ml
+++ src/util/debug.ml
@@ -9,6 +9,7 @@ let rec string_of_expr = function
 	| Mul -> "*"
 	| Div -> "/") ^ " " ^
 	string_of_expr e2
+  | Seq(l, r) -> (string_of_expr l) ^ "\n" ^ (string_of_expr r)
   | Lit(l) -> string_of_int l
   | Asn(i, e) -> "<var:" ^ string_of_int i ^ "> = " ^ string_of_expr e
   | Var(i) -> "<var:" ^ string_of_int i ^ ">"
