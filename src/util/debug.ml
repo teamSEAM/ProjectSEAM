4cc661130809b2a66a1a6423486bc5b0dd38b7bc
Akira Baruah <akira.baruah@gmail.com>
2015-11-01 06:20:42 +0000

    Clean up Debug.string_of_expr formatting

--- src/util/debug.ml
+++ src/util/debug.ml
@@ -1,15 +1,8 @@
 open Ast
 
 let rec string_of_expr = function
-    Binop(e1, op, e2) ->
-      string_of_expr e1 ^ " " ^
-	(match op with
-	  Add -> "+"
-	| Sub -> "-"
-	| Mul -> "*"
-	| Div -> "/") ^ " " ^
-	string_of_expr e2
+    Binop(e1, op, e2) -> string_of_expr e1 ^ " <Binop> " ^ string_of_expr e2
   | Seq(l, r) -> (string_of_expr l) ^ "\n" ^ (string_of_expr r)
-  | Lit(l) -> string_of_int l
-  | Asn(i, e) -> "<var:" ^ string_of_int i ^ "> = " ^ string_of_expr e
-  | Var(i) -> "<var:" ^ string_of_int i ^ ">"
+  | Lit(l) -> "<Lit:" ^ string_of_int l ^ ">"
+  | Asn(i, e) -> "<Var:" ^ string_of_int i ^ "> = " ^ string_of_expr e
+  | Var(i) -> "<Var:" ^ string_of_int i ^ ">"
