4d80ae96cc854dbf64c4f974a7b846bf0d0d5dda
Akira Baruah <akira.baruah@gmail.com>
2015-11-01 06:09:54 +0000

    Test driver prints out parsed tokens rather than evaluating expr

--- src/util/test.ml
+++ src/util/test.ml
@@ -21,5 +21,4 @@ let rec eval = function
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let expr = Parser.expr Scanner.token lexbuf in
-  let result = eval expr in
-  print_endline (string_of_int result)
+  print_endline (Debug.string_of_expr expr)
