dd3cce024b1ec5ee78982e1c0782009f7b2d213f
Edmund Qiu <edmundjqiu@gmail.com>
2015-11-11 00:54:06 -0500

    fleshed out ast

--- src/core/ast.mli
+++ src/core/ast.mli
@@ -1,5 +1,11 @@
 type operator = Add | Sub | Mul | Div | Equal | Neq | Less | Leq | Greater| Geq 
 
+
+type primitive_type = Bool | Str | Instance | Int
+
+(* bool is whether this is an array *)
+type actual_type = primitive_type * bool
+
 type expr =
   | Binop of expr * operator * expr
   | Lit of int
@@ -8,7 +14,31 @@ type expr =
   | Call of string * expr list
   | Noexpr (* SEAN *)
 
-type stmt=
+type stmt =
   |Expr of expr (* EXPR ; *)
   |Block of stmt list (* { ... } SEAN *)
   |If of expr * stmt * stmt (* SEAN *)
+
+
+type v_decl = actual_type * string
+type func_decl = {
+        fname: string;
+        return_type: actual_type;
+        formals: v_decl list;
+        locals: v_decl list;
+        body: stmt list;
+}
+
+type entity_decl = {
+        ename: string;
+        members: v_decl list; 
+        functions: func_decl list;
+}
+
+type toplevel_element = func_decl | v_decl | entity_decl
+type program = toplevel_element list
+
+
+
+
+
