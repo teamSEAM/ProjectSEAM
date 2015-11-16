f82a994d0b9e0b4af6801aba641a91b0fec4b837
Maclyn Brandwein <maclyn@maclyn.me>
2015-11-16 00:31:58 -0500

    Successfully linked in semantic.ml

--- src/core/seam.ml
+++ src/core/seam.ml
@@ -3,4 +3,5 @@ type action = Ast | Interpret | Bytecode | Compile
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
+  let verified = Semantic.check program in
   print_endline "Hello?"
---
+++ src/core/semantic.ml
@@ -0,0 +1,10 @@
+(*
+  * To analyze: 
+  *     Return types
+  *     Expression type checks 
+  *     Undeclared identifiers being used
+  *     Unset identifiers being used (to an extent)
+*)
+
+let check prog = 
+    prog
