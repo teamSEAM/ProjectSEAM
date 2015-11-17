5bcd53db669b01531d7e9631155ef35217aaeb0c
Edmund Qiu <edmundjqiu@gmail.com>
2015-11-16 23:50:21 -0500

    updated test case

--- src/core/gen.c
+++ src/core/gen.c
@@ -1,3 +1,3 @@
 #include "lib.h"
 #include "gen.h"
-void program_ep () { _seam_print( "Hello world!" ); }
+void program_ep () { _seam_print( "Hello world!" ); _seam_print( "We can print two lines too." ); _seam_print( "Edwards, do we get full credit now?" ); }
