bf5a8230a624057ce70144243537265410f3eaf3
Akira Baruah <akira.baruah@gmail.com>
2015-12-21 23:36:00 +0000

    scratch/: pretty printing version (no compilation)

--- scratch/seam.ml
+++ scratch/seam.ml
@@ -3,4 +3,5 @@ type action = Ast | Compile
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
-  Compile.translate program
+  print_string Ast.string_of_program program
+  (* Compile.translate program *)
