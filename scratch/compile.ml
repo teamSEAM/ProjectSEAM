802aaa44cd87976dd8a4bc76728f7e967b63b2bf
Akira Baruah <akira.baruah@gmail.com>
2015-12-22 10:46:21 +0000

    compile.ml: compiles, but does not work

--- scratch/compile.ml
+++ scratch/compile.ml
@@ -1,104 +1,129 @@
 open Ast
-open Bytecode
 
-module StringMap = Map.Make(String)
+exception UndeclaredEntity of string
+exception UndeclaredVariable of string
 
-(* Symbol table: Information about all the names in scope *)
-type env = {
-    function_index : int StringMap.t; (* Index for each function *)
-    global_index   : int StringMap.t; (* "Address" for global variables *)
-    local_index    : int StringMap.t; (* FP offset for args, locals *)
-  }
+type symbol_table = {
+  parent : symbol_table option;
+  current_entity : edecl;
+  variables : vdecl list;
+}
 
-(* val enum : int -> 'a list -> (int * 'a) list *)
-let rec enum stride n = function
-    [] -> []
-  | hd::tl -> (n, hd) :: enum stride (n+stride) tl
+type environment = {
+  entities : edecl list;
+  scope : symbol_table;
+}
 
-(* val string_map_pairs StringMap 'a -> (int * 'a) list -> StringMap 'a *)
-let string_map_pairs map pairs =
-  List.fold_left (fun m (i, n) -> StringMap.add n i m) map pairs
+let find_entity (env : environment) name =
+  try List.find (fun e -> e.ename = name) env.entities
+  with Not_found -> raise (UndeclaredEntity name)
 
-(** Translate a program in AST form into a bytecode program.  Throw an
-    exception if something is wrong, e.g., a reference to an unknown
-    variable or function *)
-let translate (globals, entities, functions) =
+let rec find_variable (scope : symbol_table) name =
+  try List.find (fun (_, n) -> n = name) scope.variables
+  with Not_found ->
+    match scope.parent with
+      Some(parent) -> find_variable parent name
+    | _ -> raise (UndeclaredVariable name)
 
-  (* Allocate "addresses" for each global variable *)
-  let global_indexes = string_map_pairs StringMap.empty (enum 1 0 globals) in
+let add_edecl env edecl = {
+  entities = edecl :: env.entities;
+  scope = {
+    parent = None;
+    current_entity = edecl;
+    variables = edecl.fields;
+  };
+}
 
-  (* Assign indexes to function names; built-in "print" is special *)
-  let built_in_functions = StringMap.add "print" (-1) StringMap.empty in
-  let function_indexes = string_map_pairs built_in_functions
-      (enum 1 1 (List.map (fun f -> f.fname) functions)) in
+let add_scope env vdecls = {
+  entities = env.entities;
+  scope = {
+    parent = Some(env.scope);
+    current_entity = env.scope.current_entity;
+    variables = vdecls;
+  };
+}
 
-  (* Translate a function in AST form into a list of bytecode statements *)
-  let translate env fdecl =
-    (* Bookkeeping: FP offsets for locals and arguments *)
-    let num_formals = List.length fdecl.formals
-    and num_locals = List.length fdecl.locals
-    and local_offsets = enum 1 1 fdecl.locals
-    and formal_offsets = enum (-1) (-2) fdecl.formals in
-    let env = { env with local_index = string_map_pairs
-		  StringMap.empty (local_offsets @ formal_offsets) } in
+let rec tr_expr env = function
+  | Literal(lit) -> string_of_literal lit
+  | Id(id) -> string_of_identifier id
+  | Binop(e1, o, e2) ->
+    (tr_expr env) e1 ^ " " ^ string_of_op o ^ " " ^ (tr_expr env) e2
+  | Assign(id, e) -> string_of_identifier id ^ " = " ^ (tr_expr env) e
+  | Access(id, e) -> string_of_identifier id ^ "[" ^ (tr_expr env) e ^ "]"
+  | Call(id, args) ->
+    string_of_identifier id ^
+      "(" ^ String.concat ", " (List.map (tr_expr env) args) ^ ")"
+  | Noexpr -> ""
 
-    let rec expr = function
-	Literal i -> [Lit i]
-      | Id s ->
-	  (try [Lfp (StringMap.find s env.local_index)]
-          with Not_found -> try [Lod (StringMap.find s env.global_index)]
-          with Not_found -> raise (Failure ("undeclared variable " ^ s)))
-      | Binop (e1, op, e2) -> expr e1 @ expr e2 @ [Bin op]
-      | Assign (s, e) -> expr e @
-	  (try [Sfp (StringMap.find s env.local_index)]
-  	  with Not_found -> try [Str (StringMap.find s env.global_index)]
-	  with Not_found -> raise (Failure ("undeclared variable " ^ s)))
-      | Call (fname, actuals) -> (try
-	  (List.concat (List.map expr (List.rev actuals))) @
-	  [Jsr (StringMap.find fname env.function_index) ]
-        with Not_found -> raise (Failure ("undefined function " ^ fname)))
-      | Noexpr -> []
+let rec tr_stmt env = function
+  | Block(stmts) ->
+    "{\n" ^ String.concat "" (List.map (tr_stmt env) stmts) ^ "}\n"
+  | Expr(expr) -> (tr_expr env) expr ^ ";\n";
+  | Return(expr) -> "return " ^ (tr_expr env) expr ^ ";\n";
+  | If(e, s, Block([])) ->
+    "if (" ^ (tr_expr env) e ^ ")\n" ^ (tr_stmt env) s
+  | If(e, s1, s2) ->
+    "if (" ^ (tr_expr env) e ^ ")\n" ^ (tr_stmt env) s1 ^
+      "else\n" ^ (tr_stmt env) s2
+  | For(e1, e2, e3, s) ->
+    "for (" ^ (tr_expr env) e1  ^ " ; " ^ (tr_expr env) e2 ^ " ; " ^
+      (tr_expr env) e3  ^ ") " ^ (tr_stmt env) s
+  | While(e, s) -> "while (" ^ (tr_expr env) e ^ ") " ^ (tr_stmt env) s
 
-    in let rec stmt = function
-	Block sl     ->  List.concat (List.map stmt sl)
-      | Expr e       -> expr e @ [Drp]
-      | Return e     -> expr e @ [Rts num_formals]
-      | If (p, t, f) -> let t' = stmt t and f' = stmt f in
-	expr p @ [Beq(2 + List.length t')] @
-	t' @ [Bra(1 + List.length f')] @ f'
-      | For (e1, e2, e3, b) ->
-	  stmt (Block([Expr(e1); While(e2, Block([b; Expr(e3)]))]))
-      | While (e, b) ->
-	  let b' = stmt b and e' = expr e in
-	  [Bra (1+ List.length b')] @ b' @ e' @
-	  [Bne (-(List.length b' + List.length e'))]
+let tr_vdecl (typ, name) =
+  string_of_dtype typ ^ " " ^ name ^ ";"
 
-    in [Ent num_locals] @      (* Entry: allocate space for locals *)
-    stmt (Block fdecl.body) @  (* Body *)
-    [Lit 0; Rts num_formals]   (* Default = return 0 *)
+let tr_fdecl env fdecl =
+  let env = add_scope env (fdecl.formals @ fdecl.locals) in
+  let ename = env.scope.current_entity.ename in
+  let mangled_fname = "__" ^ ename ^ "_" ^ fdecl.fname in
+  let first_arg = "struct " ^ ename ^ " *this" in
+  let rtype = fdecl.rtype in
+  string_of_rtype rtype ^ mangled_fname ^
+    "(" ^ String.concat ", " (first_arg :: List.map string_of_formal fdecl.formals) ^
+    ") {\n" ^ String.concat "\n" (List.map tr_vdecl fdecl.locals) ^ "\n" ^
+    String.concat "\n" (List.map (tr_stmt env) fdecl.body)
 
-  in let env = { function_index = function_indexes;
-		 global_index = global_indexes;
-		 local_index = StringMap.empty } in
+let tr_edecl (env, output) edecl =
+  let env = add_edecl env edecl in
+  let ename = edecl.ename in
+  let fields = List.map tr_vdecl edecl.fields in
+  let methods = List.map (tr_fdecl env) edecl.methods in
+  let translated = "struct " ^ ename ^ " {\n" ^
+    String.concat "\n" fields ^ "\n}\n\n" ^
+    String.concat "\n" methods ^ "\n" in
+  (env, translated :: output)
 
-  (* Code executed to start the program: Jsr main; halt *)
-  let entry_function = try
-    [Jsr (StringMap.find "main" function_indexes); Hlt]
-  with Not_found -> raise (Failure ("no \"main\" function"))
-  in
+let translate entities =
+  let empty_edecl = { ename = ""; fields = []; methods = [] } in
+  let empty_env = {
+    entities = [];
+    scope = { parent = None; current_entity = empty_edecl; variables = [] };
+  } in
+  let (env, translated) = (List.fold_left tr_edecl (empty_env, []) entities) in
+  String.concat "\n" translated
 
-  (* Compile the functions *)
-  let func_bodies = entry_function :: List.map (translate env) functions in
+(* let tr_stmt stmt = string_of_stmt stmt *)
 
-  (* Calculate function entry points by adding their lengths *)
-  let (fun_offset_list, _) = List.fold_left
-      (fun (l,i) f -> (i :: l, (i + List.length f))) ([],0) func_bodies in
-  let func_offset = Array.of_list (List.rev fun_offset_list) in
+(* let tr_vdecl (dtype, name) = *)
 
-  { num_globals = List.length globals;
-    (* Concatenate the compiled functions and replace the function
-       indexes in Jsr statements with PC values *)
-    text = Array.of_list (List.map (function
-	Jsr i when i > 0 -> Jsr func_offset.(i)
-      | _ as s -> s) (List.concat func_bodies))
-  }
+
+(* let tr_fdecl ename fdecl = *)
+(*   let first_arg = "struct " ^ ename ^ "* self" *)
+(*   and mangled_fname = "__" ^ ename ^ " " ^ fdecl.fname *)
+(*   and formals = List.map string_of_formal fdecl.formals *)
+(*   and locals = List.map tr_vdecl fdecl.locals *)
+(*   in *)
+(*   string_of_rtype fdecl.rtype ^ *)
+(*     mangled_fname ^ *)
+(*     "(" ^ String.concat ", " (first_arg :: formals) ^ ") {\n" ^ *)
+
+(*     "}\n" *)
+
+(* let tr_edecl edecl = *)
+(*   "struct " ^ edecl.ename ^ " {\n" ^ *)
+(*     String.concat "\n" (List.map string_of_vdecl edecl.fields) ^ "\n}\n\n" ^ *)
+(*     String.concat "\n" (List.map (tr_fdecl edecl.name) edecl.methods) ^ "\n" *)
+
+(* let translate entities = *)
+(*   String.concat "\n" (List.map tr_edecl entities) *)
