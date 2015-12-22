6176cd763426fd1d8995cb4daab187962d16fb81
Akira Baruah <akira.baruah@gmail.com>
2015-12-22 11:01:47 +0000

    Update Makefile and main driver to use compile module

--- scratch/seam.ml
+++ scratch/seam.ml
@@ -3,5 +3,5 @@ type action = Ast | Compile
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
-  print_string (Ast.string_of_program program)
-  (* Compile.translate program *)
+  (* print_string (Ast.string_of_program program) *)
+  print_string (Compile.translate program)
