d3d1f9306259d8b0c60293148c828c599754aceb
Maclyn Brandwein <maclyn@maclyn.me>
2015-12-22 20:34:55 -0500

    Clean up scripts/ /

--- src/seam.ml
+++ src/seam.ml
@@ -1,31 +1,23 @@
-type action = Ast | Compile
-
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
-
-
-
-    let verified = Semantic.semantic_check program in
-    try
+let _ = 
+    try 
+        let lexbuf = Lexing.from_channel stdin in
+        let program = Parser.program Scanner.token lexbuf in
+        let verified = Semantic.semantic_check program in
         let result =
             if (String.compare verified "") == 0 then
-                    Compile.translate program
+                Compile.translate program
             else
-                    (
-                        print_endline verified;
-                        exit 1;
-                    )
+                (
+                    print_endline verified;
+                    print_endline "Continuing anyways...";
+                    exit 1;
+                )
             in
         print_endline result;
     with
         Parsing.Parse_error ->
-                (
-                    print_endline "Parsing error!";
-                    exit 1;
-                )
-        | _ -> exit 1
-
-
-  (* print_string (Ast.string_of_program program) *)
-   (*  print_string (Compile.translate program)  *)
+            (
+                print_endline "Parsing error!";
+                exit 1;
+            )
+        | _ -> exit 1 
--- src/seamc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SOURCE=$1
-GEN_C=${SOURCE%.*}.c
-
-# sed: remove trailing whitespace
-sed -e 's/[ \t]*$//' -e '/^$/d' $SOURCE | ./preprocessor | ./seam #> $GEN_C
-
-rm $GEN_C
